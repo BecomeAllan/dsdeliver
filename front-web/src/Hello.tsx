7932c20080c60631386a0da6706ae841b1ca3fb3
Allan <allanvictor.almeida@gmail.com>
2021-01-08 02:26:28 -0300

    Create front-web initial file

---
+++ front-web/src/Hello.tsx
@@ -0,0 +1,20 @@
+import { type } from "os"
+import { useEffect } from "react"
+
+type Props = {
+    message: string;
+}
+
+
+function Hello({ message }: Props){
+
+    useEffect(() => {
+       // chamada para API
+    }, []);
+
+    return (
+        <h1>Hello {message}!</h1>
+    )
+}
+
+export default Hello
