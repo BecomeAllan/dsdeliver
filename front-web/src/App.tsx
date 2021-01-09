0208917e1d6bdac8c3a77d7a89fdf397adae7728
Allan <allanvictor.almeida@gmail.com>
2021-01-09 06:41:32 -0300

    Finish front web

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ToastContainer } from 'react-toastify';
 import './App.css';
 import Routes from './Routes';
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
       <Routes />
+      <ToastContainer/>
     </>
   );
 }
